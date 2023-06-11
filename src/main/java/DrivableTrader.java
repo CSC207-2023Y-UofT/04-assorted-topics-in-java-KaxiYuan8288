e7e9ff925a2e98f926b844c8f43dd949b31dfdb8
KaxiYuan8288 <123778353+KaxiYuan8288@users.noreply.github.com>
2023-06-11 16:32:17 -0400

    Update DrivableTrader.java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,26 @@
  */
 
 import java.util.List;
+
+
+public class DrivableTrader extends Trader<Drivable>{
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money){
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money){
+        super(money);
+    }
+    @Override
+    public int getSellingPrice(Drivable item){
+        if (item instanceof Tradable){
+            int speed = item.getMaxSpeed();
+            int price = ((Tradable) item).getPrice();
+            return speed + price;
+        }
+        else{
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
