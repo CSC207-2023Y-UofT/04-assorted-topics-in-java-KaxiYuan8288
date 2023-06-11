334ab73ca3c235c808aad6d6a33d04978571ebc4
KaxiYuan8288 <123778353+KaxiYuan8288@users.noreply.github.com>
2023-06-11 16:44:43 -0400

    Update DrivableTrader.java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -10,27 +10,27 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import javax.lang.model.element.ModuleElement;
 import java.util.List;
 
+class DrivableTrader extends Trader<Drivable> {
 
-public class DrivableTrader extends Trader<Drivable>{
-
-    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money){
+    public DrivableTrader(List<Drivable> inventory,
+                          List<Drivable> wishlist,
+                          int money) {
         super(inventory, wishlist, money);
     }
 
-    public DrivableTrader(int money){
+    public DrivableTrader(int money) {
         super(money);
     }
+
     @Override
     public int getSellingPrice(Drivable item) {
-        int super_price = super.getSellingPrice(item);
-
-        if (super_price == Tradable.MISSING_PRICE) {
-            return super_price;
+        int price = super.getSellingPrice(item);
+        if (price == Tradable.MISSING_PRICE) {
+            return price;
         }
-
-        return super_price + item.getMaxSpeed();
-
+        return price + item.getMaxSpeed();
     }
 }
